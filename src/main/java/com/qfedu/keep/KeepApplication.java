62d12775f9615870b9bbe8c482a680295d3fa0a2
huisongxiaomanong <976226535@qq.com>
2018-09-16 17:02:32 +0800

    Initial commit

---
+++ src/main/java/com/qfedu/keep/KeepApplication.java
@@ -0,0 +1,12 @@
+package com.qfedu.keep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeepApplication.class, args);
+    }
+}
