4b869c1e559f8e93e736949400c536ea1a313040
Ma Xin <15736969207@163.com>
2018-09-18 22:35:46 +0800

    马鑫九月十八提交

--- src/main/java/com/qfedu/keep/KeepApplication.java
+++ src/main/java/com/qfedu/keep/KeepApplication.java
@@ -1,8 +1,10 @@
 package com.qfedu.keep;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.qfedu.keep.mapper")
 @SpringBootApplication
 public class KeepApplication {
 
