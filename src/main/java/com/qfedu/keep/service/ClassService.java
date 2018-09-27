2f579a55705df3c794f9ec89ffa3111eb81a598c
lxj <https://github.com/feixiangyunxiao/mistore.git>
2018-09-27 16:48:39 +0800

    移除了后台模块

--- src/main/java/com/qfedu/keep/service/ClassService.java
+++ src/main/java/com/qfedu/keep/service/ClassService.java
@@ -12,15 +12,6 @@ public interface ClassService {
     // 展示课程的具体步骤
     PageVo showClassDetail(String sid);
 
-    // 增加课程具体步骤
-    PageVo addClassDetail(ClassDetail classDetail);
-
-    // 删除课程具体步骤
-    PageVo deleteClassDetail(String id);
-
-    // 修改课程具体步骤
-    PageVo modifyClassDetail(ClassDetail classDetail);
-
     // 参加具体的某个课程
     PageVo joinClass(String sid, HttpSession session);
 
--- src/main/java/com/qfedu/keep/service/impl/ClassTypeServiceImpl.java
+++ src/main/java/com/qfedu/keep/service/impl/ClassTypeServiceImpl.java
@@ -23,16 +23,6 @@ public class ClassTypeServiceImpl implements ClassTypeService {
     SecondClassMapper secondClassMapper;
 
     @Override
-    public PageVo addFirstClassType(FirstClass firstClass) {
-        int insert = firstClassMapper.insert(firstClass);
-        if (insert > 0) {
-            return PageVo.creatJson(4000, "保存成功", null);
-        } else {
-            return PageVo.creatJson(4001, "保存失败", null);
-        }
-    }
-
-    @Override
     public PageVo showFirstClassType() {
 
         List<FirstClass> firstClasses = firstClassMapper.selectAll();
@@ -43,58 +33,6 @@ public class ClassTypeServiceImpl implements ClassTypeService {
     public PageVo showSecondClassType(String fid) {
         List<SecondClass> secondClasses = secondClassMapper.selectByFid(Integer.valueOf(fid));
         return PageVo.creatJson(5000, "查询结束", secondClasses);
-    }
-
-    @Override
-    public PageVo addSecondClassType(SecondClass secondClass) {
-        int insert = secondClassMapper.insert(secondClass);
-        if (insert > 0) {
-            return PageVo.creatJson(4000, "保存成功", null);
-        } else {
-            return PageVo.creatJson(4001, "保存失败", null);
-        }
-    }
-
-    @Override
-    public PageVo deleteClassType(String name, int level) {
-        boolean result = false;
-        if (level == 1) {
-            int i = firstClassMapper.deleteByPrimaryKey(Integer.valueOf(name));
-            if (i > 0) {
-                result = true;
-            }
-        } else {
-            int i = secondClassMapper.deleteByPrimaryKey(Integer.valueOf(name));
-            if (i > 0) {
-                result = true;
-            }
-        }
-        if (result) {
-            return PageVo.creatJson(4000, "删除成功", null);
-        } else {
-            return PageVo.creatJson(4001, "删除失败", null);
-        }
-    }
-
-    @Override
-    public PageVo modifyFirstClassType(FirstClass firstClass) {
-        int i = firstClassMapper.updateByPrimaryKey(firstClass);
-        if (i > 0) {
-            return PageVo.creatJson(4000, "修改成功", null);
-        } else {
-            return PageVo.creatJson(4001, "修改失败", null);
-        }
-
-    }
-
-    @Override
-    public PageVo modifySecondClassType(SecondClass secondClass) {
-        int i = secondClassMapper.updateByPrimaryKey(secondClass);
-        if (i > 0) {
-            return PageVo.creatJson(4000, "修改成功", null);
-        } else {
-            return PageVo.creatJson(4001, "修改失败", null);
-        }
     }
 
     @Override
